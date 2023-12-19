4d894a72fb4f86b8c173f9b3be5d3e2c1a64c6c8
Tawnee <allprt89@gmail.com>
2023-12-19 16:31:33 -0700

    upload images to uploadthing

---
+++ components/file-upload.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  endpoint: "serverImage" | "messageFile";
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").at(-1);
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.error(error);
+      }}
+    />
+  );
+};
