a7bd20ffb1bba2ae7c087029dd368d19d26abf03
Tawnee <allprt89@gmail.com>
2023-12-29 11:49:43 -0700

    delete server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
@@ -28,6 +29,7 @@ export const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
