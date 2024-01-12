aa973609007a75fb4aadf8da109d4915cc01d981
Tawnee <allprt89@gmail.com>
2024-01-12 15:52:05 -0700

    upload message files

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,12 +2,13 @@
 
 import { useEffect, useState } from "react";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
@@ -34,6 +35,7 @@ export const ModalProvider = () => {
       <DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
+      <MessageFileModal />
     </>
   );
 };
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -11,12 +11,15 @@ export type ModalType =
   | "leaveServer"
   | "deleteServer"
   | "deleteChannel"
-  | "editChannel";
+  | "editChannel"
+  | "messageFile";
 
 interface ModalData {
   server?: Server | ServerWithMembersWithProfiles;
   channel?: Channel;
   channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
 interface ModalStore {
